d855a56dc1d95c0706894d7f815b9a72b6dc9df9
Patrick <patrick04184@gmail.com>
2025-05-07 18:16:20 +0800

    Add random function for pseudo-random number generation

--- contracts/ballot.sol
+++ contracts/ballot.sol
@@ -12,4 +12,9 @@ contract Lottery {
     require(msg.value > 0.01 ether);
     players.push(msg.sender);
   }
+
+
+  function random() private view returns (uint) {
+    return uint(keccak256(block.difficulty, now, players));
+  }
 }
