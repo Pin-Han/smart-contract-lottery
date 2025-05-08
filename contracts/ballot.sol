e8f07107b01778fc8a8402abde7d2b9dc8d970ef
Patrick <patrick04184@gmail.com>
2025-05-08 15:17:45 +0800

    Add restricted modifier and getPlayers function

--- contracts/ballot.sol
+++ contracts/ballot.sol
@@ -18,9 +18,19 @@ contract Lottery {
     return uint(keccak256(block.difficulty, now, players));
   }
 
-  function pickWinner() public {
+  function pickWinner() public restricted {
+
     uint index = random() % players.length;
     players[index].transfer(this.balance);
     players = new address[](0);
+  }
+
+  function getPlayers() public view returns (address[]){
+    return players;
+  }
+
+  modifier restricted() {
+    require(msg.sender == manager);
+    _;
   }
 }
