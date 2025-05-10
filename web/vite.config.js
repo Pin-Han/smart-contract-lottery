04511515d2cf1343af67c4a3ba972b3d0c7c42b1
Patrick <patrick04184@gmail.com>
2025-05-10 15:52:46 +0800

    setup gh page

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/smart-contract-lottery/", // 替換成你的 repository 名稱
   build: {
-    outDir: "dist",
+    outDir: resolve(__dirname, "../dist"), // 輸出到根目錄的 dist 資料夾
     assetsDir: "assets",
     sourcemap: true,
   },
